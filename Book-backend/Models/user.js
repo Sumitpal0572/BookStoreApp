c110b05fc3477ade9e38bc86c94b14d3f298f638
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-12-28 23:13:28 +0530

    user schema updated

--- Book-backend/Models/user.js
+++ Book-backend/Models/user.js
@@ -4,11 +4,13 @@ const user = mongoose.Schema({
     username: {
         type: String,
         required: true,
+        unique: true
     },
 
     email: {
         type: String,
         required: true,
+        unique: true
     },
 
     password: {
@@ -19,5 +21,37 @@ const user = mongoose.Schema({
     address: {
         type: String,
         required: true,
-    }
-})
+    },
+
+    avatar: {
+        type: String,
+        default: "https://cdn-icons-png.flaticon.com/128/3177/3177440.png",
+    },
+
+    role: {
+        type: String,
+        default: "user",
+        enum: ["user", "admin"]
+    },
+
+    favourites: [{
+        type: mongoose.Types.ObjectId,
+        ref: "books"
+    },
+    ],
+
+    cart: [{
+        type: mongoose.Types.ObjectId,
+        ref: "books"
+    },
+    ],
+
+    orders: [{
+        type: mongoose.Types.ObjectId,
+        ref: "orders"
+    },
+    ],
+}, { timestamps: true }
+);
+
+module.exports = mongoose.model("user", user)
