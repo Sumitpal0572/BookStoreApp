7f24aae5a982b11d36994a41c7b8573515e109a9
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-01-06 23:22:21 +0530

    user authentication added

---
+++ Book-backend/routes/userAuth.js
@@ -0,0 +1,11 @@
+const jwt = require("jsonwebtoken");
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (token == null) {
+        return res.status(401).json({ message: "Authentication token required" });
+    }
+    jwt.verify(token, "bookstore0572",(err,user))
+}
