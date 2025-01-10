3820e41747171d15c4409a5e76741b21723ca7e8
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-01-11 01:35:40 +0530

    books route created

--- Book-backend/app.js
+++ Book-backend/app.js
@@ -3,12 +3,14 @@ const app = express();
 require("dotenv").config()
 require("./conn/connect")
 const User = require("./routes/user.routes")
+const Books = require("./routes/book.routes")
 
 PORT = 2000;
 
 app.use(express.json())
 //routes
 app.use("/api/v1", User)
+app.use("/api/v1/", Books)
 
 
 app.listen(PORT, () => {
