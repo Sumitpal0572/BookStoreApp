616ed4355bcb8bcf2256c76c4276c96d1f02ae49
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-01-18 23:59:21 +0530

    debug the code

--- Book-backend/routes/favourite.js
+++ Book-backend/routes/favourite.js
@@ -8,6 +8,7 @@ router.put("add-book-to-fav", authenticateToken, async (req, res) => {
     try {
         const { bookid, id } = req.headers
         const userData = await User.findById(id)
+        const isBookFavourite = userData.favourites.includes(bookid)
     } catch (error) {
         res.status(500).json({ message: "Internal Server Error" })
     }
