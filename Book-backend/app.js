22231afa53812f456d6a2a0e69feab4f06357780
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-01-18 23:55:48 +0530

    favourite route created

--- Book-backend/app.js
+++ Book-backend/app.js
@@ -4,6 +4,7 @@ require("dotenv").config()
 require("./conn/connect")
 const User = require("./routes/user.routes")
 const Books = require("./routes/book.routes")
+const Favourite = require("./routes/favourite")
 
 PORT = 2000;
 
@@ -11,6 +12,8 @@ app.use(express.json())
 //routes
 app.use("/api/v1", User)
 app.use("/api/v1/", Books)
+app.use("/api/v1/", Favourite)
+
 
 
 app.listen(PORT, () => {
---
+++ Book-backend/routes/favourite.js
@@ -0,0 +1,17 @@
+const router = require("express").Router.Router();
+const User = require("../Models/user")
+const { authenticateToken } = require("./userAuth");
+
+//add book to favourites
+
+router.put("add-book-to-fav", authenticateToken, async (req, res) => {
+    try {
+        const { bookid, id } = req.headers
+        const userData = await User.findById(id)
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+})
+
+
+module.exports = router
