e361f5d89c429d55cbab34346f4026a2da6810cc
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-01-20 14:09:03 +0530

    Book added to fav

--- Book-backend/routes/favourite.js
+++ Book-backend/routes/favourite.js
@@ -4,11 +4,17 @@ const { authenticateToken } = require("./userAuth");
 
 //add book to favourites
 
-router.put("add-book-to-fav", authenticateToken, async (req, res) => {
+router.put("/add-book-to-fav", authenticateToken, async (req, res) => {
     try {
         const { bookid, id } = req.headers
         const userData = await User.findById(id)
         const isBookFavourite = userData.favourites.includes(bookid)
+        if (isBookFavourite) {
+            return res.status(200).json({ message: "Book is already in favourite" })
+        }
+        await User.findByIdAndUpdate(id, { $push: { favourites: bookid } })
+        return res.status(200).json({ message: "Book added to favourite" })
+
     } catch (error) {
         res.status(500).json({ message: "Internal Server Error" })
     }
