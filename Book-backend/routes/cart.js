b89a6247ec7c7b076df728f4fec10d14ccc32db7
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-01-24 23:47:08 +0530

    add to cart route reated

--- Book-backend/routes/cart.js
+++ Book-backend/routes/cart.js
@@ -1,3 +1,16 @@
 const router = require("express").Router()
 const User = require("../Models/user");
 const { authenticateToken } = require("./userAuth");
+
+//put add to cart
+
+router.put("/add-to-cart", authenticateToken, async (req, res) => {
+    try {
+        const { bookid, id } = req.headers
+        const userData = await User.findById(id);
+    } catch (error) {
+
+    }
+})
+
+module.exports = router
