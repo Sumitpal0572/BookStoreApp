17d9bbf99e616135cb5523388d65e984536a0717
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-01-25 22:12:03 +0530

    Book added to Cart

--- Book-backend/routes/cart.js
+++ Book-backend/routes/cart.js
@@ -8,9 +8,23 @@ router.put("/add-to-cart", authenticateToken, async (req, res) => {
     try {
         const { bookid, id } = req.headers
         const userData = await User.findById(id);
-        const isBookfavourited= userData.cart
-    } catch (error) {
+        const isBookfavourited = userData.cart.includes(bookid);
+        if (isBookfavourited) {
+            return res.json({
+                status: "Success",
+                message: "Book is already in Cart",
+            });
+        }
+        await User.findByIdAndUpdate(id, {
+            $push: { cart: bookid },
+        });
 
+        return res.json({
+            status: "Success",
+            message: "Book Added to cart",
+        });
+    } catch (error) {
+        return res.status(500).json({ message: "An occur Occured" });
     }
 })
 
