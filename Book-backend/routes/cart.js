55bec8a77b6895e7801ba3047cee56132a99b966
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-01-27 23:41:12 +0530

    book remove from cart route

--- Book-backend/routes/cart.js
+++ Book-backend/routes/cart.js
@@ -28,4 +28,23 @@ router.put("/add-to-cart", authenticateToken, async (req, res) => {
     }
 })
 
+// remove the book from cart
+
+router.put("/remove-from-cart/:bookid", authenticateToken, async (req, res) => {
+    try {
+        const { bookid } = req.headers
+        const { id } = req.headers
+        await User.findByIdAndUpdate(id, {
+            $pull: { cart: bookid },
+        });
+
+        return res.json({
+            status: "Success",
+            message: "Book remove from Cart",
+        });
+    } catch (error) {
+        return res.status(500).json({ message: "An occur Occured" });
+    }
+})
+
 module.exports = router
